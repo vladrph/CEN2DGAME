ba632b123120d3a1b40f9618db37ed62c2db2029
jdago <115362953+jdago@users.noreply.github.com>
2022-10-23 08:40:02 -0700

    Create RigidBody.cs
    Allows for frontward and backward movement of Zombie 2D asset

---
+++ Assets/RigidBody.cs
@@ -0,0 +1,30 @@
+//This is code based on the video 2D Zombie AI in Unity - State Machine Behaviors
+//Specific Purpose: Used to control Zombie's frontward and backward movement based on the scale of object.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehavior
+{
+    Transform target;
+    public Transform borderCheck;
+
+void Start()
+{
+    target = GameObject.FindGameObjectWithTag("Player").transform;
+}
+
+void Update ()
+{
+    if (target.position.x > transform.position.x) 
+    {
+        transform.localScale = new Vector2(0.45f, 0.45f);
+    }
+    else 
+    {
+        transform.localScale = new Vector2(-0.45, -0.45f);
+    }
+}
+
+//Script under the Enemy folder (C#)
