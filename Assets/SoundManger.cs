8d150abfcb7429ce26efa799959dfadc739126b6
jdago <115362953+jdago@users.noreply.github.com>
2022-10-24 08:09:58 -0700

    Create SoundManger.cs
    https://www.daggerhartlab.com/unity-audio-and-sound-manager-singleton-script/
    Define a component that can be utilized to play an audio clip for the 2D Player sprite and Zombie sprites' various stages (i.e. audio clip for AttackState of Zombie 2D asset).

---
+++ Assets/SoundManger.cs
@@ -0,0 +1,44 @@
+//Specific Purpose: Define a component that can be utilized to play an audio clip for the 2D Player sprite and Zombie sprites
+//This was identified based on a tutorial for Audio and Sound Manager Singleton Script - Daggerhart Lab
+
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class SoundManager : MonoBehavior 
+{
+    public AudioSource EffectsSource;
+    public AudioSource MusicSource;
+
+    public float LowPitchRange = 0.95f;
+    public float HighPitchRange = 1.05f;
+
+public static SoundManager Instance = null;
+
+private void Singleton()
+{
+    if (Instance == null)
+    {
+        Instance = this;
+    }
+    
+    else {
+        Destroy(gameObject);
+    }
+    
+    DontDestroyOnLoad (gameObject);
+}
+
+public void Play(AudioClip clip)
+{
+    EffectsSource.clip = clip;
+    EffectsSource.Play();
+}
+
+public void PlayMusic(AudioClip clip)
+{
+    MusicSource.clip = clip;
+    MusicSource.Play();
+    }
+
+} //end MonoBehavior
