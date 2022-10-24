3b4251668f30e09ebdc70dc9bd79ae01670bf477
jdago <115362953+jdago@users.noreply.github.com>
2022-10-24 07:52:17 -0700

    Update ChaseState.cs
    Changed class name and object to StateMachineBehavior

--- Assets/ChaseState.cs
+++ Assets/ChaseState.cs
@@ -5,7 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Zombie : MonoBehavior
+public class ChaseState : StateMachineBehavior
 {
     Transform target;
     Transform borderCheck;
